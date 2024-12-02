933bdf885e29d8e4cb5bfa8de9e5eb7980f19d9c
ashi-kamra <ak118@wellesley.edu>
2024-12-02 15:01:58 -0500

    adding notifying + finalizing permissioning

---
+++ server/concepts/notifying.ts
@@ -0,0 +1,38 @@
+import { ObjectId } from "mongodb";
+import DocCollection, { BaseDoc } from "../framework/doc";
+import { NotFoundError } from "./errors";
+
+interface ActionItem {
+  action: "Payment" | "Reminder" | "SOS";
+}
+
+export interface NotifyingDoc extends BaseDoc {
+  message: string;
+  sender: ObjectId;
+  recipients: ObjectId;
+  action: ActionItem;
+}
+
+export default class NotifyingConcept {
+  public readonly notifications: DocCollection<NotifyingDoc>;
+
+  constructor(collectionName: string) {
+    this.notifications = new DocCollection<NotifyingDoc>(collectionName);
+  }
+
+  async createNotification(s: ObjectId, r: ObjectId, content: string, type: ActionItem) {
+    const _id = await this.notifications.createOne({ message: content, sender: s, recipients: r, action: type });
+    return { msg: "Member successfully created!", member: await this.notifications.readOne({ _id }) };
+  }
+
+  async searchNotifications(user: ObjectId, keyword: string) {
+    const _id = await this.notifications.readOne({
+      message: { $regex: keyword, $options: "i" },
+      recipients: { $in: [user] },
+    });
+    if (!_id) {
+      throw new NotFoundError(`Notification ${_id} does not exist!`);
+    }
+    return { msg: "Notification successfully found!", notif: await this.notifications.readOne({ _id }) };
+  }
+}
