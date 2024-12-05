88285d9c7b22c2f4ee84785b54d30450f33d2e2d
Ashi Kamra <63170259+ashi-kamra@users.noreply.github.com>
2024-12-05 09:18:19 -0800

    updated notifying.ts (#13)
    * finalizing permissioning front-end
    
    * adding notifying + finalizing permissioning
    
    * initial notifying
    
    * adding routes between permissioning, homepage, and grouping
    
    * finalizing changes before PR to main
    
    * vercel bug
    
    * vercel bug
    
    * vercel bug
    
    * vercel bug
    
    ---------
    
    Co-authored-by: Angel Whipple <awhipp@mit.edu>

--- server/concepts/notifying.ts
+++ server/concepts/notifying.ts
@@ -2,14 +2,14 @@ import { ObjectId } from "mongodb";
 import DocCollection, { BaseDoc } from "../framework/doc";
 import { NotFoundError } from "./errors";
 
-interface ActionItem {
-  action: "Payment" | "Reminder" | "SOS";
+export interface ActionItem {
+  action: "Payment" | "Reminder" | "SOS" | "Invitation";
 }
 
 export interface NotifyingDoc extends BaseDoc {
   message: string;
   sender: ObjectId;
-  recipients: ObjectId;
+  recipients: ObjectId[];
   action: ActionItem;
 }
 
@@ -20,15 +20,14 @@ export default class NotifyingConcept {
     this.notifications = new DocCollection<NotifyingDoc>(collectionName);
   }
 
-  async createNotification(s: ObjectId, r: ObjectId, content: string, type: ActionItem) {
+  async createNotification(s: ObjectId, r: ObjectId[], content: string, type: ActionItem) {
     const _id = await this.notifications.createOne({ message: content, sender: s, recipients: r, action: type });
     return { msg: "Member successfully created!", member: await this.notifications.readOne({ _id }) };
   }
 
-  async searchNotifications(user: ObjectId, keyword: string) {
-    const _id = await this.notifications.readOne({
-      message: { $regex: keyword, $options: "i" },
-      recipients: { $in: [user] },
+  async getNotifications(user: ObjectId) {
+    const _id = await this.notifications.readMany({
+      recipients: { user },
     });
     if (!_id) {
       throw new NotFoundError(`Notification ${_id} does not exist!`);
